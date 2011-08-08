6aa74211734fab48b8753ad1f3f20630c65ded08
El Skwid (Don Morrison) <elskwid@gmail.com>
2011-08-07 22:34:34 -0700

    Version 0.2.3

--- psstrip.rb
+++ psstrip.rb
@@ -76,7 +76,7 @@ end
 # Script
 #   sets up arguments, logging level, and options. Also handles help output.
 class Script
-  VERSION = '0.2.2'
+  VERSION = '0.2.3'
 
   # Simple log formatter
   class Formatter < Logger::Formatter
